8e057d68855bcf7d1c604898b88eb6396f5d1891
YOGASWETHASANJAYGANDHI <23f2000098@ds.study.iitm.ac.in>
2025-02-16 21:24:17 +0530

    Update datagen.py

--- datagen.py
+++ datagen.py
@@ -42,7 +42,7 @@ This is a sample paragraph with extra  spaces and trailing whitespace.
 - First item  
 - Second item  
     + Third item  
-    *    Fourth item  
+    -    Fourth item  
 
 ```py
 print("{email}")
